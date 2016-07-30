b0f0371315bb058902f71e5cd334518341a214cc
artembelaev <artembelaev@ya.ru>
2016-07-30 23:11:44 +0500

    Плавное изменение вектора скорости с сохранением фазы движения

--- juk.h
+++ juk.h
@@ -5,84 +5,6 @@
 #include <math.h>
 #include "adv_math.h"
 
-/// Вычисление положения ноги при перемещении прямо
-Vector4 stepLeg(
-    double t,               // Текущее время (мс) 
-    double t0,              // Время начала движения (мс)
-    double phase,           // Задержка по фазе (безразмерное в диапазоне [0, 1])
-    const Vector4 & point0, // Координаты базовой точки ноги - середина основания треугольнияка (мм)
-    const Vector4 & v,      // Скорость перемещения жука (мм/с)
-    double step_l,          // Длина шага - длина основания треугольника (мм)
-    double step_h)          // Высота шага - высота треугольника над базовой точкой (мм) 
-{
-    double v_abs = v.length();
-    double period =  2 * step_l  / v_abs * 1000; // Период шага (мc)
-    t -= t0;              // время с начала движения
-    t += phase * period;  // с учетом фазы
-    t = fmod(t, period);  // время с начала текущего периода 
-    
-    Vector4 delta01 = v  / v_abs * step_l / 2;
-
-    double t1 = period / 4;
-    double t2 = period / 2;
-    double t3 = period / 4 * 3;
-    
-    Vector4 point1 = point0 - delta01;
-    Vector4 point2(point0.x(), point0.y(), point0.z() - step_h);
-    Vector4 point3 = point0 + delta01;
-    
-    if (t <= t1) return mapVector(t, 0, t1, point0, point1);
-    else if (t > t1 && t <= t2) return mapVector(t, t1, t2, point1, point2);
-    else if (t > t2 && t < t3) return mapVector(t, t2, t3, point2, point3);
-    else return mapVector(t, t3, period, point3, point0);
-}
-
-/// Вычисление положения ноги при повороте вокруг оси oZ
-Vector4 turnLeg(
-    double t,               // Текущее время (мс) 
-    double t0,              // Время начала движения (м)
-    double phase,           // Задержка по фазе (безразмерное в диапазоне [0, 1])
-    const Vector4 & point0, // Координаты базовой точки ноги - середина основания треугольнияка (мм)
-    double w,               // Угловая скорость поворота жука по оси oZ (г/с)
-    double ampl,            // Амплитуда поворота кончика ноги (мм)
-    double step_h)          // Высота шага - высота треугольника над базовой точкой (мм) 
-{
-    double period =  4000 * ampl / fabs(w); // Период шага (мc)
-
-    t -= t0;              // время с начала движения
-    t += phase * period;  // с учетом фазы
-    t = fmod(t, period);  // время с начала текущего периода 
-
-    double t1 = period / 4;
-    double t2 = period / 2;
-    double t3 = period / 4 * 3;
-
-    if (w < 0) ampl *= -1;
-    
-    double h = 0;
-    double alpha = 0;
-    if (t <= t1)
-    {
-        alpha = mapDouble(t, 0, t2, 0, -ampl);
-    }
-    else if (t > t1 && t <= t2) 
-    {
-        alpha = mapDouble(t, t1, t2, -ampl, 0);
-        h = mapDouble(t, t1, t2, 0, step_h);
-    }
-    else if (t > t2 && t < t3) 
-    {
-        alpha = mapDouble(t, t2, t3, 0, ampl);
-        h = mapDouble(t, t2, t3, step_h, 0);
-    }
-    else 
-    {
-        alpha = mapDouble(t, t3, period, ampl, 0);
-    }
-    return rotateZ(point0, alpha) - Vector4(0, 0, h);
-    
-}
-
 Leg leg_l1(Leg::Left,   0,  1,  2, Vector4( 35, 90), - 3.5, -6.0, -4);    
 Leg leg_l2(Leg::Left,   3,  4,  5, Vector4( 35,  0), - 8.0, +6.0, +5.0);    
 Leg leg_l3(Leg::Left,   6,  7,  8, Vector4( 35,-90), -10.0, -6.0, -5.0);    
@@ -98,8 +20,6 @@ Leg leg_r3(Leg::Right,  9, 10, 11, Vector4(-35, -90), - 7.5, -7.0, -9.0);
     Порядок: сначала левые, потом правые
 */
 Leg * legs[LEG_COUNT]; 
-
-unsigned long t_start = 0;
 
 void setupLegs()
 {
@@ -175,20 +95,115 @@ void warm()
   }
 }
 
-void stepAllLegs(double azimuth, double t_start)
+/// Текущая фаза шага - число в диапазоне  [0, 1)
+double getStepPhase(double phase_prev,
+                    double dt, 
+                    double period)
 {
-    for (int i = 0; i < LEG_COUNT; ++i)
-    {
-        unsigned long t = millis();    
-        double phase = (i%2 == 0) ? 0.0 : 0.5; // смещение по фазе для правых ног
-        Leg * leg = legs[i];
-
-        double v_abs = 40; // мм/с
-        Vector4 v(v_abs * sinDeg(azimuth), v_abs * cosDeg(azimuth));
-        
-        leg->moveTo(stepLeg(t, t_start, phase, leg->tipPoint(), v, 40, 35));
-    }
+    dt = fmod(dt, period);  // время с начала текущего периода 
+    double phase = phase_prev + dt/period;
+    return phase - floor(phase); // приводим  к диапазону [0, 1)
 }
+
+double shiftPhase(double phase, double d_phase)
+{
+    return fmod(phase + d_phase, 1.0);
+}
+
+/// Вычисление положения ноги при перемещении по прямой
+Vector4 stepLeg(
+    double phase,           // Фаза шага (безразмерное в диапазоне [0, 1])
+    double period,          // Период шага
+    const Vector4 & point0, // Координаты базовой точки ноги - середина основания треугольнияка (мм)
+    const Vector4 & delta01,// точка максимального отклонения ноги при толчке назад
+    double step_h)          // Высота шага - высота треугольника над базовой точкой (мм) 
+{
+    double t = phase * period;
+    double t1 = period / 4;
+    double t2 = period / 2;
+    double t3 = period / 4 * 3;
+    
+    Vector4 point1 = point0 + delta01;
+    Vector4 point2(point0.x(), point0.y(), point0.z() - step_h);
+    Vector4 point3 = point0 - delta01;
+    
+    if (t <= t1) return mapVector(t, 0, t1, point0, point1);
+    else if (t > t1 && t <= t2) return mapVector(t, t1, t2, point1, point2);
+    else if (t > t2 && t < t3) return mapVector(t, t2, t3, point2, point3);
+    else return mapVector(t, t3, period, point3, point0);
+}
+
+/// Движение в заданном направлении
+double stepAllLegs(const Vector4 & v, 
+                   double phase_prev,
+                   double dt)
+{
+    // double step_l = 40; // Длина шага - длина основания треугольника (мм)
+    // double period =  2 * step_l  / v_abs * 1000;        // Период шага (мc)
+    // Vector4 delta01 = v  / v_abs * step_l / -2;
+    
+    double period = 1200;
+    double v_abs = v.length();
+    double phase = getStepPhase(phase_prev, dt, period);
+    Vector4 delta01 = v * period / -4000; 
+
+    double step_h = min(delta01.length()*1.7, 40);
+    
+    for (int i = 0; i < LEG_COUNT; ++i)
+    { 
+        double d_phase = (i%2 == 0) ? 0.0 : 0.5; // смещение по фазе для правых ног
+        Leg * leg = legs[i];
+        leg->moveTo(stepLeg(shiftPhase(phase, d_phase), period, leg->tipPoint(), delta01, step_h));
+    }
+    return phase;
+}
+
+/// Вычисление положения ноги при повороте вокруг оси oZ
+Vector4 turnLeg(
+    double t,               // Текущее время (мс) 
+    double t0,              // Время начала движения (м)
+    double phase,           // Задержка по фазе (безразмерное в диапазоне [0, 1])
+    const Vector4 & point0, // Координаты базовой точки ноги - середина основания треугольнияка (мм)
+    double w,               // Угловая скорость поворота жука по оси oZ (г/с)
+    double ampl,            // Амплитуда поворота кончика ноги (мм)
+    double step_h)          // Высота шага - высота треугольника над базовой точкой (мм) 
+{
+    double period =  4000 * ampl / fabs(w); // Период шага (мc)
+
+    t -= t0;              // время с начала движения
+    t += phase * period;  // с учетом фазы
+    t = fmod(t, period);  // время с начала текущего периода 
+
+    double t1 = period / 4;
+    double t2 = period / 2;
+    double t3 = period / 4 * 3;
+
+    if (w < 0) ampl *= -1;
+    
+    double h = 0;
+    double alpha = 0;
+    if (t <= t1)
+    {
+        alpha = mapDouble(t, 0, t2, 0, -ampl);
+    }
+    else if (t > t1 && t <= t2) 
+    {
+        alpha = mapDouble(t, t1, t2, -ampl, 0);
+        h = mapDouble(t, t1, t2, 0, step_h);
+    }
+    else if (t > t2 && t < t3) 
+    {
+        alpha = mapDouble(t, t2, t3, 0, ampl);
+        h = mapDouble(t, t2, t3, step_h, 0);
+    }
+    else 
+    {
+        alpha = mapDouble(t, t3, period, ampl, 0);
+    }
+    return rotateZ(point0, alpha) - Vector4(0, 0, h);
+    
+}
+
 /*Vector4 turnLeg(
     double t,               // Текущее время (мс) 
     double t0,              // Время начала движения (м)
--- juk.ino
+++ juk.ino
@@ -3,23 +3,8 @@
 #include "juk.h"
 
 IRrecv irrecv(11); // указываем вывод, к которому подключен приемник
-double azimuth = 0;
-IrXindaRemoteCode last_it_code = ircUp;
 
-void setup() 
-{
-    Serial.begin(9600);
-    
-
-    setupLegs();
-
-    t_start = millis();
-    irrecv.enableIRIn(); // запускаем прием
-
-    // warm();
-
-    //allLegsSet90();
-}
+IrXindaRemoteCode last_ir_code = ircUp;
 
 void getAzimuth(double & a, IrXindaRemoteCode code, IrXindaRemoteCode & last_code)
 {
@@ -38,24 +23,23 @@ void getAzimuth(double & a, IrXindaRemoteCode code, IrXindaRemoteCode & last_cod
     
 }
 
+double phase_prev = 0;
+double t_prev = 0;
+double azimuth = 0;
+unsigned long t_start = 0;
+
 /// Плавное изменение направления шага ноги в сторону наименьшего поворота
-void smoothAzimuth(double dt, double a_start, double a_end, double t_start)
-{
-    a_start = normalizeAngle(a_start);
-    a_end = normalizeAngle(a_end);
-    if (a_end < a_start - 180)
-        a_end += 360;
-    else if (a_end > a_start + 180)
-        a_end -= 360;
-    
+void smoothV(double dt, const Vector4 & v_start, const Vector4 & v_end, double & phase_prev, double & t_prev)
+{    
     double t_min = millis();
     double t = t_min;
     double t_max = t_min + dt;
     while (t < t_max)
     {
-        double azimuth = mapDouble(t, t_min, t_max, a_start, a_end);
-        stepAllLegs(normalizeAngle(azimuth), t_start);
         t = millis();
+        Vector4 v = mapVector(t, t_min, t_max, v_start, v_end);
+        phase_prev = stepAllLegs(v, phase_prev, t - t_prev);
+        t_prev = t;
     }
 }
 
@@ -71,27 +55,53 @@ void turn(double dt, double w, double ampl)
     }
 }
 
+
+void setup() 
+{
+    Serial.begin(9600);
+    
+
+    setupLegs();
+
+    t_start = millis();
+    t_prev = t_start;
+    irrecv.enableIRIn(); // запускаем прием
+
+    // warm();
+
+    //allLegsSet90();
+}
+
+Vector4 getV(double azimuth, double v_abs)
+{
+    return Vector4(v_abs * sinDeg(azimuth), v_abs * cosDeg(azimuth));
+}
+
 void loop() 
 {
     //// Хождение по кругу
     // stepAllLegs(double(millis())/8000.0 * 360);
-
- /*   decode_results ir_result;
+    double v_abs = 70; // мм/с
+    decode_results ir_result;
     if (irrecv.decode(&ir_result))
     {
+        
         double new_azimuth = azimuth;
-        getAzimuth(new_azimuth, getXindaCode(ir_result.value), last_it_code);
+        getAzimuth(new_azimuth, getXindaCode(ir_result.value), last_ir_code);
         if (fabs(new_azimuth - azimuth) > 1.0)
         {
-            smoothAzimuth(700, azimuth, new_azimuth, t_start);
+            smoothV(800, getV(azimuth, v_abs), getV(new_azimuth, v_abs), phase_prev, t_prev);
         }
-        irrecv.resume(); // принимаем следующую команду        
+        irrecv.resume(); // принимаем следующую команду
         azimuth = new_azimuth;
     }
-    stepAllLegs(azimuth, t_start);  */
+    
+    double t = millis();
+    phase_prev = stepAllLegs(getV(azimuth, v_abs), phase_prev, t - t_prev);
+    t_prev = t;
 
-    turn(5000, 25, 10);
-    turn(5000, -25, 10);
+   // turn(5000, 25, 10);
+    //turn(5000, -25, 10);
     
   
 }
